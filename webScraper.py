2db1b168d3f616217835d24792801c66eac94391
Alex Wong <wongaz@rose-hulman.edu>
2018-01-09 16:23:17 -0500

    damn no white space commit

---
+++ webScraper.py
@@ -0,0 +1,19 @@
+from urllib.request import *
+from bs4 import BeautifulSoup
+import os
+
+baseurl = "http://www.dot.state.mn.us/traffic/data/reports-hrvol-atr.html"
+website = "http://www.dot.state.mn.us"
+soup = BeautifulSoup(urlopen(baseurl))
+links=soup.find_all("a", href = True)
+for link in links:
+    #print(link['href'])
+    if (link.text == "TXT"):
+        temp = link['href'].split("Hourly_Volume")
+        print(website+link['href'])
+        last_two=str(temp[-1])
+        first=last_two.split('/')
+        directory = first[1]
+        if not os.path.exists('./'+directory):
+            os.makedirs('./'+directory)
+        urlretrieve(website+link['href'], "."+str(temp[-1]))
